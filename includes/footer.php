8ce9571ccf50a6c1a17331a89831da5aaed229d5
ybaras <yoav@gizra.com>
2016-09-24 15:14:26 +0300

    Add page structure.

---
+++ includes/footer.php
@@ -0,0 +1,66 @@
+</div>
+<div id="footer">
+    <div class="ui grid three column stackable">
+        <div class="column">
+            <div class="ui horizontal icon divider">
+                <i class="circular list icon"></i>
+            </div>
+            <div class="ui list">
+                <a href="index.php" class="item">
+                    <i class="home icon"></i>
+                    Home
+                </a>
+                <a href="about.php" class="item">
+                    <i class="user icon"></i>
+                    About
+                </a>
+                <a href="portfolio.php" class="item">
+                    <i class="archive icon"></i>
+                    Portfolio
+                </a>
+                <a href="contact.php" class="item">
+                    <i class="mail icon"></i>
+                    Contact
+                </a>
+            </div>
+        </div>
+        <div class="column">
+            <div class="ui horizontal icon divider">
+                <i class="circular chat icon"></i>
+            </div>
+            <p>Proin vitae laoreet nisl, faucibus cursus lectus. Sed vehicula vel lectus nec porttitor. Vestibulum
+                scelerisque sollicitudin justo a sollicitudin.</p>
+        </div>
+        <div class="column">
+            <div class="ui horizontal icon divider">
+                <i class="circular pencil icon"></i>
+            </div>
+            <div class="ui feed small">
+                <div class="event">
+                    <div class="label">
+                        <img src="images/person.jpg" alt="Person"/>
+                    </div>
+                    <div class="content">
+                        <div class="date">Just Now</div>
+                        <div class="summary"><a href="#">Hello World</a> Blog Post</div>
+                    </div>
+                </div>
+                <div class="event">
+                    <div class="label">
+                        <img src="images/person.jpg" alt="Person"/>
+                    </div>
+                    <div class="content">
+                        <div class="date">Just Now</div>
+                        <div class="summary"><a href="#">Hello World</a> Blog Post</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-3.1.1.min.js"><\/script>')</script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+<script type="text/javascript" src="css/ui/semantic.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
